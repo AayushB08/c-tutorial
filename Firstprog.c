f4b96de3e874ccd3eb017fa03ea304524e136c96
AayushB08 <119524629+AayushB08@users.noreply.github.com>
2022-12-03 12:58:57 +0530

    Create Firstprog.c

---
+++ Firstprog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  printf("Nayi duniya m swagat hai\n");
+  int x = 4;
+  int y = 6;
+  int sum = x+y;
+  printf("%i",sum);
+  return 0;
+}
